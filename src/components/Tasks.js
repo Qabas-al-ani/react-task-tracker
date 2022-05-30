4e3556b1128cb26741eb0941ac4077abc638a0d3
Qabas <qabasalani1@gmail.com>
2022-05-30 14:54:19 -0400

    refacter my react

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,24 +1,26 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors appointment",
-    day: "feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Meeting at school",
-    day: "feb 6th at 1:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "food shopping",
-    day: "feb 5th at 2:30pm",
-    reminder: false,
-  },
-];
+import { useState } from "react";
+
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors appointment",
+      day: "feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "food shopping",
+      day: "feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
   return (
     <>
       {tasks.map(task => (
