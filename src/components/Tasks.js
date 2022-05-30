b4420b97b564d75fe1810831af37d0aed883398d
Qabas <qabasalani1@gmail.com>
2022-05-30 14:49:38 -0400

    refacter my react

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -6,20 +6,26 @@ const tasks = [
     reminder: true,
   },
   {
-    id: 1,
+    id: 2,
     text: "Meeting at school",
     day: "feb 6th at 1:30pm",
     reminder: true,
   },
   {
-    id: 1,
+    id: 3,
     text: "food shopping",
     day: "feb 5th at 2:30pm",
     reminder: false,
   },
 ];
 const Tasks = () => {
-  return <div>Tasks</div>;
+  return (
+    <>
+      {tasks.map(task => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
