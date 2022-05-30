f49475297bc20bea9e61835978c2224010e0fa0a
Qabas <qabasalani1@gmail.com>
2022-05-30 14:41:23 -0400

    refacter my react

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,23 @@
-import React from "react";
-
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors appointment",
+    day: "feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: "Meeting at school",
+    day: "feb 6th at 1:30pm",
+    reminder: true,
+  },
+  {
+    id: 1,
+    text: "food shopping",
+    day: "feb 5th at 2:30pm",
+    reminder: false,
+  },
+];
 const Tasks = () => {
   return <div>Tasks</div>;
 };
