e4e5a809ef9fb4cdde2477d7bd9a43bcd26becbe
Qabas <qabasalani1@gmail.com>
2022-05-30 17:39:12 -0400

    add new component

--- src/App.js
+++ src/App.js
@@ -27,12 +27,15 @@ function App() {
   const deleteTask = id => {
     setTasks(tasks.filter(tasks => tasks.id !== id));
   };
+  const toggleReminder = id => {
+    console.log(id);
+  };
 
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         " No Tasks To Show"
       )}
